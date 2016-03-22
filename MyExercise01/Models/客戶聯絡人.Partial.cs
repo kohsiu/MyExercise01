2e83ded0890cdfd9218c23d051cdab6f959d1ee4
kohsiu <chen.kohsiu@gmail.com>
2016-03-22 17:11:18 +0800

    新增手機格式驗證錯誤訊息

--- MyExercise01/Models/客戶聯絡人.Partial.cs
+++ MyExercise01/Models/客戶聯絡人.Partial.cs
@@ -61,7 +61,7 @@ namespace MyExercise01.Models
         public string Email { get; set; }
 
         [StringLength(50, ErrorMessage = "欄位長度不得大於 50 個字元")]
-        [手機格式驗證]
+        [手機格式驗證(ErrorMessage = "手機格式錯誤")]
         public string 手機 { get; set; }
 
         [StringLength(50, ErrorMessage = "欄位長度不得大於 50 個字元")]
